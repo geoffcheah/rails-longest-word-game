8ed4bc475da883577fa17c6aba132211d7d5ad03
Geoffrey Cheah <geoffcheah@gmail.com>
2018-02-19 16:08:39 +0000

    got the logic to work. created methods to check grid, if its english word and to calculate score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   LETTERS = ("A".."Z").to_a
 
@@ -9,5 +12,40 @@ class GamesController < ApplicationController
   end
 
   def score
+    score = 0
+    @message = ""
+    word = params[:word]
+    # word_array = word.upcase.split("")
+    word_array = word.upcase.split("")
+    grid = params[:grid].split(" ")
+    if grid_check?(word_array, grid)
+      if is_word?(word)
+        score = calculate_score(word)
+        @message = "Congratulations!! You scored #{score}"
+      else
+        @message = "Not a valid english word"
+      end
+    else
+      @message = "Sorry but #{word.upcase} can't be build out of #{grid}"
+    end
+  end
+
+  def grid_check?(word_array, grid)
+    length = word_array.length
+    word_array.all? do |letter|
+      grid.include?(letter) && word_array.count(letter) <= grid.count(letter) && length > 1
+    end
+  end
+
+  def is_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    word_serialized = open(url).read
+    word = JSON.parse(word_serialized)
+    return word["found"]
+  end
+
+  def calculate_score(word)
+    length = word.length
+    return length * 10
   end
 end
